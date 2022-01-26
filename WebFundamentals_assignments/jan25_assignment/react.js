cb0700d2e7c4cc855992d72dfe087bf9c7c76113
Akhil-6 <96535688+Akhil-6@users.noreply.github.com>
2022-01-26 15:05:51 +0530

    Add files via upload

---
+++ WebFundamentals_assignments/jan25_assignment/react.js
@@ -0,0 +1,24 @@
+//Assignment - 1
+
+var a = prompt("enter a number");
+if(a>10) {
+	console.log(a + " is greater than 10")}
+else if(a==10) {
+	console.log(a + " is equal to 10")}
+else  {
+	console.log(a + " is neither greater than nor equal to 10")}
+
+
+// Assigment - 2
+
+var num1 = prompt("enter a num1");
+var num2 = prompt("enter a num2");
+var num3 = prompt("enter a num3"); 
+if (num1>num2 && num1>num3) {
+	console.log("num1 is greater")}
+else if(num2>num1 && num2>num3) {
+	console.log("num2 is greater")}
+else {
+	console.log("num3 is greater")}
+
+	
