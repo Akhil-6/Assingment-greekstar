5079a01fa322a44fc087298445247913063cf4d7
Akhil-6 <96535688+Akhil-6@users.noreply.github.com>
2022-01-31 11:05:16 +0530

    Update index.js

--- WebFundamentals_assignments/jan27_assignment/index.js
+++ WebFundamentals_assignments/jan27_assignment/index.js
@@ -8,6 +8,33 @@ for(var i = 0;i<input;i++) {
 	arr.push(num);
 }
 console.log(arr)
+
+//bubble sort
+
+for(var i=0;i<=input-1;i++){
+	var count = 0;
+	for(var j=0;j<input-1;j++)
+	{
+		a=arr[j];
+		b=arr[j+1]
+		if(a<b)
+		{
+			arr[j]=b;
+			arr[j+1]=a;
+			count=count+1;
+		}
+	}
+	console.log(arr);
+	if(count==0){
+		break;
+	}
+	
+}
+console.log(arr[0]);	
+alert("maximum of given numbers is " + arr[0] );	
+
+/* without sorting
+
 var max = arr[0];
 for(var i=1;i<input;i++) {
 	if(max<=arr[i]) {
@@ -16,7 +43,7 @@ for(var i=1;i<input;i++) {
 }
 alert("maximum of given numbers is " + max );
 console.log(max);
-
+*/
 
 
 		
