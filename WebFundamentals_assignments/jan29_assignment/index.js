c6b5d23148c9d9155db44d47b99bce27a0aa5736
Akhil-6 <96535688+Akhil-6@users.noreply.github.com>
2022-02-01 13:09:30 +0530

    Update index.js

--- WebFundamentals_assignments/jan29_assignment/index.js
+++ WebFundamentals_assignments/jan29_assignment/index.js
@@ -1,34 +1,41 @@
 //Assignment -> 1 give count of vowels in a number
 
 var name = prompt("enter your name");
-var arr = name.split("");
-console.log(arr)
-var count = 0;
-for(var i =0;i<arr.length;i++){
-	switch(arr[i]){
-		case 'e':
-		case 'E':
-			count=count+1;
-			break;
-		case 'a':
-		case 'A':
-			count=count+1;
-			break;
-		case 'i':
-		case 'I':
-			count=count+1;
-			break;
-		case 'o':
-		case 'O':
-			count=count+1;
-			break;
-		case 'u':
-		case 'U':
-			count=count+1;
-			break;
+if(isNaN(name)){
+	var arr = name.split("");
+	console.log(arr)
+	var count = 0;
+	for(var i =0;i<arr.length;i++){
+		switch(arr[i]){
+			case 'e':
+			case 'E':
+				count=count+1;
+				break;
+			case 'a':
+			case 'A':
+				count=count+1;
+				break;
+			case 'i':
+			case 'I':
+				count=count+1;
+				break;
+			case 'o':
+			case 'O':
+				count=count+1;
+				break;
+			case 'u':
+			case 'U':
+				count=count+1;
+				break;
+		}
 	}
-}console.log("Number of vowels in your name is " + count);
-
+	console.log("Number of vowels in your name is " + count);
+	alert("Number of vowels in your name are " + count);
+}
+else {
+	console.log("enter a valid name");
+	alert("enter a valid name");
+}
 
 //Assignment -> 2 check given number is prime num or not
 
@@ -45,9 +52,9 @@ for(var i=1;i<=num/2;i++){
 if(isNaN(num))
 	alert("enter a valid number");
 else if (sum==1){
-	console.log("is prime");
-	alert("is prime");}
+	console.log(num + " is prime");
+	alert(num + "is prime");}
 else {
-	console.log("is not aprime");
+	console.log(num + " is not aprime");
 	alert(num + " is not a prime");
 }
