2501f1904fb30081a58b5e653c6f060341a88769
Akhil-6 <96535688+Akhil-6@users.noreply.github.com>
2022-02-06 16:41:35 +0530

    Update react.js

--- WebFundamentals_assignments/jan25_assignment/react.js
+++ WebFundamentals_assignments/jan25_assignment/react.js
@@ -2,15 +2,18 @@
 
 var a = prompt("enter a number");
 if(a>10) {
+	alert(a + " is greater than 10")
 	console.log(a + " is greater than 10")}
 else if(a==10) {
+	alert(a + " is equal to 10")
 	console.log(a + " is equal to 10")}
 else  {
+	alert(a + " is neither greater than nor equal to 10")
 	console.log(a + " is neither greater than nor equal to 10")}
 
 
 // Assigment - 2
-
+alert("To find greatest of three numbers")
 var num1 = prompt("enter a num1");
 var num2 = prompt("enter a num2");
 var num3 = prompt("enter a num3"); 
